a9c92e951d06201d09e4f06a0d3bcaeb0ae34020
燕文强 <yanwq@centaline.com.cn>
2019-04-23 13:29:51 +0800

    Update TestPaper.java

--- basics/src/arithmetic/TestPaper.java
+++ basics/src/arithmetic/TestPaper.java
@@ -2,10 +2,7 @@ package arithmetic;
 
 import core.MainLaunch;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 /**
  * 描述：待描述
@@ -19,66 +16,32 @@ public class TestPaper extends MainLaunch {
     public void start() {
         Option bgOption1 = new Option("左边", 10);
         Option bgOption2 = new Option("右边", 7);
-        Option bgOption3 = new Option("上边", 2);
+        Option bgOption3 = new Option("上边", 12);
         Option bgOption4 = new Option("下边", 5);
-        Option[] options = new Option[]{bgOption1, bgOption2, bgOption3, bgOption4};
-
+        List<Option> options = new ArrayList<Option>(Arrays.asList(bgOption1, bgOption2, bgOption3, bgOption4));
         List<String> symbols = new ArrayList<String>(Arrays.asList("A", "B", "C", "D"));
 
         int count = symbols.size();
         for (int i = 0; i < count; i++) {
             int index = new Random().nextInt(symbols.size());
-            options[i].symbol = symbols.get(index);
+            options.get(i).symbol = symbols.get(index);
             symbols.remove(index);
         }
 
-
-        for (int i = 0; i < options.length; i++) {
-            System.out.println(options[i]);
+        options.sort(Comparator.naturalOrder());
+        for (int i = 0; i < options.size(); i++) {
+            System.out.println(options.get(i));
         }
 
     }
 
-    class Option {
+    class Option implements Comparable<Option> {
         private String symbol; // A、B、C、D
         private String value; // 选项内容
         private int score; // 得分
 
-        public Option() {
-        }
-
         public Option(String value, int score) {
             this.value = value;
-            this.score = score;
-        }
-
-        public Option(String symbol, String value, int score) {
-            this.symbol = symbol;
-            this.value = value;
-            this.score = score;
-        }
-
-        public String getSymbol() {
-            return symbol;
-        }
-
-        public void setSymbol(String symbol) {
-            this.symbol = symbol;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
-
-        public int getScore() {
-            return score;
-        }
-
-        public void setScore(int score) {
             this.score = score;
         }
 
@@ -89,6 +52,15 @@ public class TestPaper extends MainLaunch {
                     ", value='" + value + '\'' +
                     ", score=" + score +
                     '}';
+        }
+
+        @Override
+        public int compareTo(Option o) {
+            char c1 = this.symbol.charAt(0);
+            char c2 = o.symbol.charAt(0);
+            int thisASC = Integer.valueOf(c1);
+            int targetASC = Integer.valueOf(c2);
+            return thisASC - targetASC;
         }
     }
 }
