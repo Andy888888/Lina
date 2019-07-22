393aca71e5b42b3b20e12dbee5c3d09067bf96b0
燕文强 <yanwq@centaline.com.cn>
2019-07-22 12:08:37 +0800

    Convert completed

--- basics/src/integer/Convert.java
+++ basics/src/integer/Convert.java
@@ -3,6 +3,7 @@ package integer;
 import core.MainLaunch;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -16,7 +17,8 @@ public class Convert extends MainLaunch {
 
     @Override
     public void start() {
-        convert2(30);
+        System.out.println(convert2(255));
+        System.out.println(convert10("11111111"));
     }
 
     private String convert2(int number) {
@@ -28,20 +30,66 @@ public class Convert extends MainLaunch {
         list.add(curNumber);
 
         while (curNumber < number) {
-            curNumber = curNumber * 2;
+            curNumber = curNumber << 1;
             list.add(curNumber);
         }
 
+        int plus = 0;
+        Collections.reverse(list);
+        StringBuilder resb = new StringBuilder();
         for (Integer integer : list) {
-            System.out.println(integer);
+            boolean isPlus = tryPlus(plus, integer, number);
+            if (isPlus) {
+                resb.append('1');
+                plus += integer;
+                continue;
+            }
+            resb.append('0');
         }
 
+        int index = resb.indexOf("1");
+        String result = resb.substring(index, resb.length());
 
-        return "";
+        return result;
 
     }
 
     private String convert10(String number) {
-        return "";
+        int length = number.length();
+        int[] sequence = sequence(length);
+
+        int plus = 0;
+        for (int i = length - 1; i >= 0; i--) {
+            if (number.substring(i, i + 1).equals("1")) {
+                plus += sequence[i];
+            }
+        }
+        return String.valueOf(plus);
+    }
+
+    private boolean tryPlus(int a, int b, int tag) {
+        int c = a + b;
+        if (c <= tag) {
+            return true;
+        }
+        return false;
+    }
+
+    private int[] sequence(int length) {
+        int[] sequence;
+        if (length <= 0) {
+            return null;
+        }
+
+        sequence = new int[length];
+        sequence[0] = 1;
+        if (length == 1) {
+            return sequence;
+        }
+
+        for (int i = 1; i < length; i++) {
+            sequence[i] = sequence[i - 1] << 1;
+        }
+        return sequence;
     }
 }
